07655c5245a54dbe797c83b879b4652fe416b2c7
kasunmendis7 <tkomendis@gmail.com>
2024-08-13 10:42:28 +0530

    caeser cipher completed with logic

--- caeser-cipher/CaesarCipher.scala
+++ caeser-cipher/CaesarCipher.scala
@@ -1,41 +1,47 @@
-import scala.io.StdIn.readLine;
+import scala.io.StdIn.readLine
 object CaesarCipher {
   def encrypt(text: String, shift: Int): String = {
-    text.map{char=>
-      if(char.isLetter){
-        val base = if(char.isUpper) 'A' else 'a'
-        ((char - base + shift)%26+base).toChar
-      }else{
-        char
-      }
-    }
-  }
-
-  def decrypt(text: String, shift: Int): String = {
+    val normalizedShift = shift % 26
     text.map { char =>
       if (char.isLetter) {
         val base = if (char.isUpper) 'A' else 'a'
-        ((char - base - shift + 27) % 26 + base).toChar
+        ((char - base + normalizedShift+26) % 26 + base).toChar
       } else {
         char
       }
     }
   }
 
-  def cipherFunction(text: String, shift: Int, operation: (String, Int)=>String): String = {
-    operation(text, shift);
+  def decrypt(text: String, shift: Int): String = {
+    val normalizedShift = shift % 26
+    text.map { char =>
+      if (char.isLetter) {
+        val base = if (char.isUpper) 'A' else 'a'
+        ((char - base - normalizedShift + 26) % 26 + base).toChar
+      } else {
+        char
+      }
+    }
+  }
+
+  def cipherFunction(text: String, shift: Int, operation: (String, Int) => String): String = {
+    operation(text, shift)
   }
 
   def main(args: Array[String]): Unit = {
     print("Enter the message: ")
-    var message = readLine();
+    val message = readLine()
     print("Enter the shift value: ")
-    var shift = readLine().toInt;
-    
+    val shift = readLine().toInt
+
     val encryptedMessage = cipherFunction(message, shift, encrypt)
     println(s"Encrypted Message: $encryptedMessage")
 
-    val decryptedMessage = cipherFunction(message, shift, decrypt)
-    println(s"Decrypted Message: $decryptedMessage")
+    print("Press 1 to decrypt the message: ")
+    val num = readLine().toInt
+    if (num == 1) {
+      val decryptedMessage = cipherFunction(encryptedMessage, shift, decrypt)
+      println(s"Decrypted Message: $decryptedMessage")
+    }
   }
 }
